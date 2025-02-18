da580dda1eeb6548fca425574e3099ba732477fe
Usaid Aka <usaid.anshorulloh@phincon.com>
2025-02-18 21:12:43 +0700

    layout and card service

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,14 +12,31 @@ const page = () => {
       <div>
         <h1>banner identity</h1>
       </div>
-      <div>
-        <h1>item list</h1>
+      <div className={classes.service}>
+        <div className={classes.title}>
+          <h1>We can carry the items you need</h1>
+        </div>
+        <div className={classes.wrapper}>
+        <div className={classes.top}>
+          <div className={classes.first}>
+            <h1>1</h1>
+          </div>
+          <div className={classes.second}>
+            <h1>2</h1>
+          </div>
+        </div>
+        <div className={classes.bellow}>
+          <div className={classes.third}>
+            <h1>3</h1>
+          </div>
+          <div className={classes.fourth}>
+            <h1>4</h1>
+          </div>
+        </div>
+        </div>
       </div>
       <div>
         <h1>quotes</h1>
-      </div>
-      <div>
-        <h1>footer</h1>
       </div>
     </div>
   );
